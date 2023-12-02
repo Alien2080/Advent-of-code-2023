ee56b5ebe188fc246ec0ac1b0cee35a6816f9035
Alien2080 <tom_aylen@hotmail.com>
2023-12-02 15:09:28 +1030

    refactor

--- Advent of Code 2023/source/day 1/day1.cs
+++ Advent of Code 2023/source/day 1/day1.cs
@@ -4,86 +4,74 @@ namespace solutions
     {
         public static void day_1_1()
         {
-            // Move the current directory up 3 places.
-            Directory.SetCurrentDirectory(@"..\..\..");
-            string filePath = @"source\day 1\input.txt";
+            string filePath = @"..\..\..\source\day 1\input.txt";
             var input = File.ReadAllText(filePath);
-
             string[] lines = input.Split(Environment.NewLine);
 
             int total = 0;
-            int firstNumber;
-            int secondnumber;
-            bool foundFirst;
+            int? firstNumber;
+            int? secondnumber;
+
             foreach (string line in lines)
             {
-                foundFirst = false;
-                firstNumber = 0;
-                secondnumber = 0;
-
-                char[] letters = line.ToCharArray();
+                firstNumber = null;
+                secondnumber = null;
+                char[] letters = line.ToCharArray();   
 
                 foreach (char letter in letters)
                 {
                     if (char.IsNumber(letter) == true)
                     {
-                        if (!foundFirst)
-                        {
-                            foundFirst = true;
-                            firstNumber = int.Parse(letter.ToString());
-                            secondnumber = firstNumber;
-                        } 
-                        else 
-                        {
-                            secondnumber = int.Parse(letter.ToString());
-                        }
+                        firstNumber ??= int.Parse(letter.ToString());
+                        secondnumber = int.Parse(letter.ToString());
                     }     
                 } 
                 total += int.Parse(firstNumber.ToString() + secondnumber.ToString());
             }
-            Console.WriteLine("total: " + total);
+            Console.WriteLine("Day 1, puzzle 1: " + total);
         }
 
-        public static void day_1_2()
-        {
-            // Move the current directory up 3 places.
-            Directory.SetCurrentDirectory(@"..\..\..");
-            string filePath = @"source\day 1\input.txt";
-            var input = File.ReadAllText(filePath);
+        // public static void day_1_2()
+        // {
+        //     // Move the current directory up 3 places.
+        //     Directory.SetCurrentDirectory(@"..\..\..");
+        //     string filePath = @"source\day 1\input.txt";
+        //     var input = File.ReadAllText(filePath);
 
-            string[] lines = input.Split(Environment.NewLine);
+        //     string[] lines = input.Split(Environment.NewLine);
 
-            int total = 0;
-            int firstNumber;
-            int secondnumber;
-            bool foundFirst;
-            foreach (string line in lines)
-            {
-                foundFirst = false;
-                firstNumber = 0;
-                secondnumber = 0;
+        //     int total = 0;
+        //     int firstNumber;
+        //     int secondnumber;
+        //     bool foundFirst;
+        //     foreach (string line in lines)
+        //     {
+        //         foundFirst = false;
+        //         firstNumber = 0;
+        //         secondnumber = 0;
 
-                char[] letters = line.ToCharArray();
+        //         char[] letters = line.ToCharArray();
 
-                foreach (char letter in letters)
-                {
-                    if (char.IsNumber(letter) == true)
-                    {
-                        if (!foundFirst)
-                        {
-                            foundFirst = true;
-                            firstNumber = int.Parse(letter.ToString());
-                            secondnumber = firstNumber;
-                        } 
-                        else 
-                        {
-                            secondnumber = int.Parse(letter.ToString());
-                        }
-                    }     
-                } 
-                total += int.Parse(firstNumber.ToString() + secondnumber.ToString());
-            }
-            Console.WriteLine("total: " + total);
-        }
+        //         foreach (char letter in letters)
+        //         {
+        //             if (char.IsNumber(letter) == true)
+        //             {
+        //                 if (!foundFirst)
+        //                 {
+        //                     foundFirst = true;
+        //                     firstNumber = int.Parse(letter.ToString());
+        //                     secondnumber = firstNumber;
+        //                 } 
+        //                 else 
+        //                 {
+        //                     secondnumber = int.Parse(letter.ToString());
+        //                 }
+        //             }
+        //             else if (char)     
+        //         } 
+        //         total += int.Parse(firstNumber.ToString() + secondnumber.ToString());
+        //     }
+        //     Console.WriteLine("total: " + total);
+        // }
     }
 }
